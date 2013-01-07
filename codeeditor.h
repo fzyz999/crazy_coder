14e156bb39359eb9239850cf580250bd661e776f
Luming Wang <wlm199558@126.com>
2013-01-07 13:12:31 +0800

    update some notes accomplished line number bar

--- codeeditor.h
+++ codeeditor.h
@@ -1,19 +1,62 @@
 #ifndef CODEEDITOR_H
 #define CODEEDITOR_H
 
+#include <QPainter>
 #include <QPlainTextEdit>
+#include <QTextBlock>
 
-class CodeEditor : public QPlainTextEdit
+
+class CodeEditorConfig
 {
 public:
-    CodeEditor();
-    CodeEditor(QWidget *parent);
+    static void setLineumAreaColor(QColor &color); //font color will be complute automatically
+    static const QColor& getLineumAreaBackgroundColor();
+    static const QColor& getLineumAreaFontColor();
 
 private:
-    QWidget *linenumarea;
+    CodeEditorConfig(); //the member functions in this class is like global functions in C program language.
+    static QColor linenumAreaBackgroundColor;
+    static QColor linenumAreaFontColor;
+};
+
+class CodeEditor;
+
+class LinenumArea : public QWidget
+{
+
+public:
+    LinenumArea(CodeEditor *parent);
+    void setAreaWidth(const int &w);
+    QSize sizeHint() const {
+        return QSize(areaWidth, 0);
+    }
+    int getAreaWidth();
 
 protected:
     void paintEvent(QPaintEvent *e);
+
+private:
+    int areaWidth;
+    CodeEditor *editor;
+};
+
+class CodeEditor : public QPlainTextEdit
+{
+    Q_OBJECT
+
+public:
+    CodeEditor(QWidget *parent);
+    void lineNumPaintEvent(QPaintEvent *e);
+
+protected:
+    void resizeEvent(QResizeEvent *e);
+
+private slots:
+    void lineNumberAreaWidth();
+    void updateLineNumberArea(const QRect &rect, int dy);
+
+private:
+    LinenumArea *lineNumArea;
 };
 
 #endif // CODEEDITOR_H
--- note/doxygen-note.md
+++ note/doxygen-note.md
@@ -1,5 +1,8 @@
 #简介#
-Doxygen是一个将源代码中的特殊注释转化为文档（参考手册）的工具。
+　　Doxygen是一个将源代码中的特殊注释转化为文档（参考手册）的工具。
+
+#官方文档#
+　　[在线文档](http://www.doxygen.nl/manual/index.html)
 
 #可处理语言#
 C/C++
@@ -25,5 +28,33 @@ Unix Man Page
 >Ubuntu: sudo apt-get install doxygen
 
 #使用#
+　　doxygen会将按特殊格式书写的注释转为文档
+##注释风格##
+　　通常代码可以附上一个注释块来对代码进行解释，一个注释块由一行或者多行组成。通常一个注释块包括一个简要说明（brief）和一个详细说明（detailed），这两部分都是可选的。 可以有多种方式标识出doxygen可识别的注释块。
+>1.JavaDoc类型的多行注释。
+>>/**
+>>/...text...
+>>*/
+>2.QT样式的多行注释。
+>>/*!
+>>/...text...
+>>*/
+>3.使用至少两行c++注释块，在每行开始处加斜杠或惊叹号
+>>///
+>>/// …text….
+>>///
+>或
+>>//!
+>>//! …text….
+>>//!
 
+##简明描述##
+　　简明描述有多种可行方式
+　　在注释块头部使用\brief命令。这个命令在自然段结束时结束，空一行后紧接详细描述。例如：
+>/*! \brief Brief description.
+>*         Brief description continued.
+>*
+>*  Detailed description starts here.
+>*/
+　　其余方式详见doxygen文档。
 
