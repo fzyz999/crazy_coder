a8b7b18a14444891f82989efdc0202e5d654dd76
Luming Wang <wlm199558@126.com>
2013-02-07 16:53:51 +0800

    translate file update

---
+++ src/zh_CN.ts
@@ -0,0 +1,284 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>CodeEditor</name>
+    <message>
+        <location filename="codeeditor.cpp" line="205"/>
+        <source>Save File</source>
+        <translation>保存文件</translation>
+    </message>
+    <message>
+        <location filename="codeeditor.cpp" line="267"/>
+        <source>The document has been modified.</source>
+        <translation>文件已被修改。</translation>
+    </message>
+    <message>
+        <location filename="codeeditor.cpp" line="268"/>
+        <source>Do you want to save your changes?</source>
+        <translation>是否保存修改过的文件？</translation>
+    </message>
+    <message>
+        <location filename="codeeditor.cpp" line="281"/>
+        <source>Saving file failed!</source>
+        <translation>保存文件失败！</translation>
+    </message>
+    <message>
+        <location filename="codeeditor.cpp" line="282"/>
+        <source>Please save it again</source>
+        <translation>请重新保存文件</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="33"/>
+        <source>&amp;new</source>
+        <translation>新建(&amp;n)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="34"/>
+        <source>&amp;open</source>
+        <translation>打开(&amp;o)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>&amp;save</source>
+        <translation>保存(&amp;s)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="37"/>
+        <source>save all</source>
+        <oldsource>&amp;save all</oldsource>
+        <translation>保存全部文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>&amp;quit</source>
+        <translation>退出(&amp;q)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="41"/>
+        <source>&amp;Edit</source>
+        <translation>编辑(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>&amp;undo</source>
+        <translation>撤销(&amp;u)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="43"/>
+        <source>&amp;redo</source>
+        <translation>重做(&amp;r)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="45"/>
+        <source>&amp;cut</source>
+        <translation>剪切(&amp;c)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>&amp;copy</source>
+        <translation>拷贝(&amp;c)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>&amp;paste</source>
+        <translation>粘贴(&amp;p)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>&amp;preference</source>
+        <translation>首选项(&amp;p)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>&amp;Build</source>
+        <translation>构建(&amp;B)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <source>&amp;compile</source>
+        <translation>编译(&amp;c)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="54"/>
+        <source>&amp;Help</source>
+        <translation>帮助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>&amp;about</source>
+        <translation>关于(&amp;a)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="56"/>
+        <location filename="mainwindow.cpp" line="91"/>
+        <source>about Qt</source>
+        <translation>关于Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>Crazy Coder is a light IDE for OIer/ACMer</source>
+        <translation>Crazy Coder 是一个专为OIer/ACMer设计的轻量级IDE</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>preference</source>
+        <translation>首选项</translation>
+    </message>
+</context>
+<context>
+    <name>TabManager</name>
+    <message>
+        <location filename="tabmanager.cpp" line="25"/>
+        <source>Open File</source>
+        <translation>打开文件</translation>
+    </message>
+    <message>
+        <location filename="tabmanager.cpp" line="62"/>
+        <source>Untitled</source>
+        <translation>未命名</translation>
+    </message>
+    <message>
+        <location filename="tabmanager.cpp" line="66"/>
+        <source>out of memmory!</source>
+        <translation>内存不足！</translation>
+    </message>
+    <message>
+        <location filename="tabmanager.cpp" line="78"/>
+        <location filename="tabmanager.cpp" line="100"/>
+        <source>Saving file failed!</source>
+        <translation>保存文件失败！</translation>
+    </message>
+</context>
+<context>
+    <name>compilerDockWidget</name>
+    <message>
+        <location filename="comipler.cpp" line="38"/>
+        <source>compile finished
+</source>
+        <translation>编译完毕
+</translation>
+    </message>
+    <message>
+        <location filename="comipler.cpp" line="40"/>
+        <source>compiler crashed
+ exit code </source>
+        <translation type="unfinished">编译器崩溃
+ exit code </translation>
+    </message>
+</context>
+<context>
+    <name>editorConfigDlg</name>
+    <message>
+        <location filename="editorconfigdlg.ui" line="14"/>
+        <source>EditorConfig</source>
+        <translation>编辑器设置</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="41"/>
+        <source>Font</source>
+        <translation>字体</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="60"/>
+        <source>Font Size</source>
+        <translation>字号</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="91"/>
+        <source>keywords</source>
+        <translation>关键字</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="99"/>
+        <source>numbers</source>
+        <translation>数字</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="107"/>
+        <source>comments</source>
+        <translation>注释</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="115"/>
+        <source>preprocess</source>
+        <translation>预处理</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="123"/>
+        <source>strings</source>
+        <translation>字符串</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="141"/>
+        <source>Blod</source>
+        <translation>加粗</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="153"/>
+        <source>Italic</source>
+        <translation>倾斜</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="160"/>
+        <source>Foreground Color</source>
+        <translation>颜色</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="200"/>
+        <source>preview</source>
+        <translation>预览</translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="207"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Abyssinica SIL&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;//the preview of source highlight effect&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;#include &amp;lt;iostream&amp;gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;using namespace std;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;class helloWorld&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;{&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;public:&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	helloWorld();&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	~helloWorld();&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	void display();&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;};&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;inline helloWorld::display()&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;{&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	cout&amp;lt;&amp;lt;&amp;quot;hello world!&amp;quot;&amp;lt;&amp;lt;endl;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;}&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;int main()&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;{&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	helloWorld example;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	example.display();&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;	return 0;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;}&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="editorconfigdlg.ui" line="251"/>
+        <source>Reset All</source>
+        <translation>全部重置</translation>
+    </message>
+</context>
+</TS>
